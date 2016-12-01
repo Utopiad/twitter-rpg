09aa6cdfc0718ad432c6f7dff8e8f4473761358e
Utopiad <jeremy.philippe@hetic.net>
2016-12-01 17:45:09 +0100

    adding rspec and devise
    Repair

---
+++ app/controllers/yolo_controller.rb
@@ -0,0 +1,5 @@
+class YoloController < ApplicationController
+  def index
+    @hello = "Hello Suckers!"
+  end
+end
---
+++ spec/controllers/yolo_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe YoloController, type: :controller do
+
+end
