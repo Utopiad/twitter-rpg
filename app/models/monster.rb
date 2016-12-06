993387f307242aba65f7d95764c5de10c8c6235d
Ziserman <ziserman@MacBook-Pro-de-Ziserman.local>
2016-12-06 21:04:22 +0100

    make monsters fight again

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,4 +1,27 @@
 class Monster < ApplicationRecord
   include Combat
   belongs_to :world
+  has_many :fights, as: :attacker
+  has_many :fights, as: :defender
+
+  def malus_life=(malus_life)
+    self[:malus_life] = malus_life
+    self.save
+  end
+
+  def life
+    self.life - self.malus_life
+  end
+
+  def armor
+    self.armor
+  end
+
+  def attack_min
+    self.attack_min
+  end
+
+  def attack_max
+    self.attack_max
+  end
 end
