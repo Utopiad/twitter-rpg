93fda7eea0e60927b0789998a0f4402842081129
Ziserman <martinziserman@gmail.com>
2016-12-08 00:18:14 +0100

    migration

---
+++ db/migrate/20161207193327_create_messages.rb
@@ -0,0 +1,11 @@
+class CreateMessages < ActiveRecord::Migration[5.0]
+  def change
+    create_table :messages do |t|
+      t.integer :character_id
+      t.integer :world_id
+      t.text :message
+
+      t.timestamps
+    end
+  end
+end
