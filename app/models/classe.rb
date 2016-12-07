b428379a382c53f215ad00fedd2909c0abd6cb19
Ziserman <martinziserman@gmail.com>
2016-12-07 01:00:04 +0100

    test classe model. validation

--- app/models/classe.rb
+++ app/models/classe.rb
@@ -1,4 +1,9 @@
 class Classe < ApplicationRecord
   belongs_to :world
   has_many :characters, dependent: :destroy
+  validates :name, presence: true
+  validates :attack_min, presence: true
+  validates :attack_max, presence: true
+  validates :armor, presence: true
+  validates :life, presence: true
 end
---
+++ test/models/classe_test.rb
@@ -0,0 +1,88 @@
+require 'test_helper'
+
+class ClasseTest < ActiveSupport::TestCase
+
+  test "classe can be created" do
+    assert Classe.new
+  end
+
+  test "classe can be named" do
+    name = "name"
+    classe = Classe.new(name: name)
+    assert_equal(classe.name, name)
+  end
+
+  test "classe can be saved" do
+    name = "name"
+    u = User.new(email: "martin@gmail.com", password: "aaaaaa")
+    u.save
+    w = World.new(user: u, name: name)
+    w.save
+    classe = Classe.new(world: w, name: name, attack_min: 1,
+      attack_max: 1, armor: 1, life: 1)
+    assert classe.save
+  end
+
+  test "classe can be fetched" do
+    name = "name"
+    u = User.new(email: "martin@gmail.com", password: "aaaaaa")
+    u.save
+    w = World.new(user: u, name: name)
+    w.save
+    classe = Classe.new(world: w, name: name, attack_min: 1,
+      attack_max: 1, armor: 1, life: 1)
+    classe.save
+    fetched_classe = Classe.where(id: classe.id).first
+    assert_equal(fetched_classe.name, classe.name)
+  end
+
+  test "classe shouldn't be saved without a user" do
+    classe = Classe.new(name: name)
+    assert_not classe.save
+  end
+
+  test "classe shouldn't be saved without a name" do
+    u = User.new(email: "martin@gmail.com", password: "aaaaaa")
+    u.save
+    w = World.new(user: u, name: name)
+    w.save
+    classe = Classe.new(world: w, attack_min: 1, attack_max: 1, armor: 1, life: 1)
+    assert_not classe.save
+  end
+
+  test "classe shouldn't be saved without attack min" do
+    u = User.new(email: "martin@gmail.com", password: "aaaaaa")
+    u.save
+    w = World.new(user: u, name: name)
+    w.save
+    classe = Classe.new(world: w, attack_max: 1, armor: 1, life: 1)
+    assert_not classe.save
+  end
+
+  test "classe shouldn't be saved without attack max" do
+    u = User.new(email: "martin@gmail.com", password: "aaaaaa")
+    u.save
+    w = World.new(user: u, name: name)
+    w.save
+    classe = Classe.new(world: w, attack_min: 1, armor: 1, life: 1)
+    assert_not classe.save
+  end
+
+  test "classe shouldn't be saved without armor" do
+    u = User.new(email: "martin@gmail.com", password: "aaaaaa")
+    u.save
+    w = World.new(user: u, name: name)
+    w.save
+    classe = Classe.new(world: w, attack_min: 1, attack_max: 1, life: 1)
+    assert_not classe.save
+  end
+
+  test "classe shouldn't be saved without life" do
+    u = User.new(email: "martin@gmail.com", password: "aaaaaa")
+    u.save
+    w = World.new(user: u, name: name)
+    w.save
+    classe = Classe.new(world: w, attack_min: 1, attack_max: 1, armor: 1)
+    assert_not classe.save
+  end
+end
