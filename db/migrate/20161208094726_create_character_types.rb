e991ebb31521a6b67d9db6637c40789b4f093bc1
Ziserman <martinziserman@gmail.com>
2016-12-08 11:22:57 +0100

    migration

---
+++ db/migrate/20161208094726_create_character_types.rb
@@ -0,0 +1,14 @@
+class CreateCharacterTypes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :character_types do |t|
+      t.integer :world_id
+      t.text :name
+      t.integer :attack_min
+      t.integer :attack_max
+      t.integer :armor
+      t.integer :life
+
+      t.timestamps
+    end
+  end
+end
