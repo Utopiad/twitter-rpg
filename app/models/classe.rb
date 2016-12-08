2509849685deb11d208871f2f7d0081f5c3d12cf
Ziserman <martinziserman@gmail.com>
2016-12-08 11:58:20 +0100

    forgot some

--- app/models/classe.rb
+++
@@ -1,9 +0,0 @@
-class CharacterType < ApplicationRecord
-  belongs_to :world
-  has_many :characters, dependent: :destroy
-  validates :name, presence: true
-  validates :attack_min, presence: true
-  validates :attack_max, presence: true
-  validates :armor, presence: true
-  validates :life, presence: true
-end
