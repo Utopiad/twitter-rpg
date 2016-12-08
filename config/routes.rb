53303fe65909f58a1f58d12e53f0e9b9bd8c19a3
Ziserman <martinziserman@gmail.com>
2016-12-09 00:37:49 +0100

    routes for game

--- config/routes.rb
+++ config/routes.rb
@@ -4,24 +4,21 @@ Rails.application.routes.draw do
     resources :character
     resources :stuff
     resources :character_type
-    resources :monster do
-      resources :event_monster
+    resources :monster
+    resources :chapter do
+      resources :event do
+        resources :reward
+        resources :event_monster
+        resources :turn
+      end
     end
+    resources :game
   end
 
-  get '/game/world/:world_id', to: 'game#index'
 
-  # map.connect 'game/world/:world_id', :controller => 'game', :action => 'index'
-
-  # scope path: "game", controller: :game do
-  #   map.resources :world, :controller => "game"
-  #   resources :world do
-  #     resources :turn
-  #     resources :chapter do
-  #       resources :event
-  #     end
-  #   end
-  # end
+  get '/game/:world_id', to: 'game#index' do
+    resources :turn
+  end
 
 
 
