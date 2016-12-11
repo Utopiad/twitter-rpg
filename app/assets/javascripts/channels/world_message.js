8960fa396a7e0d0e394e1ecb03d1f48dfd7d248b
Ziserman <martinziserman@gmail.com>
2016-12-11 05:42:05 +0100

    pass data from view to cable

--- app/assets/javascripts/channels/world_message.js
+++ app/assets/javascripts/channels/world_message.js
@@ -11,15 +11,17 @@ App.world_message = App.cable.subscriptions.create("WorldMessageChannel", {
     $('#messages').append(data.message)
   },
 
-  speak: function(message) {
-    return this.perform('speak', {message: message});
+  speak: function(message, event_id, character_id) {
+    return this.perform('speak', {message: message, event_id: event_id, character_id: character_id});
   }
 });
 
 
 $(document).on('keypress', '#chat-speak', function(e) {
+  var event_id = $('#event_id').html()
+  var character_id = $('#character_id').html()
   if (event.keyCode == 13) {
-    App.world_message.speak(event.target.value)
+    App.world_message.speak(event.target.value, event_id, character_id)
     e.target.value = ""
     e.preventDefault()
   } 
--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,12 +1,9 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :current_user, :current_world, :current_event, :character
+    identified_by :current_user
 
     def connect
       self.current_user = cookies.signed[:current_user_id]
-      self.current_world = cookies.signed[:current_world_id]
-      self.current_event = cookies.signed[:current_event_id]
-      self.character = cookies.signed[:character_id]
     end
   end
 end
