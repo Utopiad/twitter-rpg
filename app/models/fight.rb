71da2735f981525117243adfd9d7f4e1b03fead7
Ziserman <martinziserman@gmail.com>
2016-12-11 23:19:23 +0100

    fetch attacker and defender before creating the message

--- app/models/fight.rb
+++ app/models/fight.rb
@@ -4,13 +4,31 @@ class Fight < ApplicationRecord
 
 
   def export(format)
+    defender = self.fetch_fighter(:defender)
+    attacker = self.fetch_fighter(:attacker)
     if format == :txt
       template = "%s à attaqué %s lui portant un coup à %i de degats qui ont
       étés reduits par son armure de %i. Il reste %i pv à %s et %i pv à
       %s"
-      return template % [self.attacker.name, self.defender.name, hit,
-        self.defender.armor, self.attacker.current_life, self.attacker.name,
-        self.defender.current_life, self.defender.name]
+      return template % [self.attacker.name, defender.name, hit,
+        defender.armor, attacker.current_life, self.attacker.name,
+        defender.current_life, self.defender.name]
+    end
+  end
+
+  def fetch_fighter(role)
+    if role == :defender
+      if self.defender_type == "Character"
+        Character.find(defender_id)
+      elsif fighter.defender_type == "EventMonster"
+        EventMonster.find(defender_id)
+      end
+    elsif role == :attacker
+      if self.attacker_type == "Character"
+        Character.find(attacker_id)
+      elsif fighter.attacker_type == "EventMonster"
+        EventMonster.find(attacker_id)
+      end
     end
   end
 end
