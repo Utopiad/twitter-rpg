1749364c9f46eee6d7e3c6205cae1266c4879abb
Ziserman <martinziserman@gmail.com>
2016-12-18 19:23:58 +0100

    default not finished for turn. Create turn on pass_turn

--- db/migrate/20161208143326_create_turns.rb
+++ db/migrate/20161208143326_create_turns.rb
@@ -2,7 +2,7 @@ class CreateTurns < ActiveRecord::Migration[5.0]
   def change
     create_table :turns do |t|
       t.integer :event_id
-      t.integer :finished
+      t.integer :finished, default: 0
 
       t.timestamps
     end
